e8296051efd19e1240c73f622ba921e25c87b10b
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2022-02-21 18:48:42 -0300

    Teste Exceptions Pensao Alimenticia

--- tp1/src/test/PensaoAlimenticia/PensaoAlimenticia.java
+++ tp1/src/test/PensaoAlimenticia/PensaoAlimenticia.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import Exceptions.DescricaoEmBrancoException;
+import Exceptions.ValorMenorOuIgualZero;
 import models.SimuladorIRPF;
 
 public class PensaoAlimenticia {
@@ -33,6 +35,18 @@ public class PensaoAlimenticia {
 		simul.cadastraPensaoAlimenticia("Pensao 1",600f);
 		simul.cadastraPensaoAlimenticia("Pensao 2",400f);
 		assertEquals(1000f, simul.getPensaoAlimenticia(),0f);
+	}
+	
+	@Test(expected = DescricaoEmBrancoException.class)
+	public void testeDescEmBranco() throws Exception {
+		simul.cadastraPensaoAlimenticia(" ", 1000f);
+		
+	}
+	
+	@Test(expected = ValorMenorOuIgualZero.class)
+	public void testeValorMenorOuIgualZero() throws Exception {
+		simul.cadastraPensaoAlimenticia(" Carnê", 0f);
+		
 	}
 
 }
