ac612d331f0efe02cb24b4059c48b3251eadfe00
Gabriel Azevedo Batalha <batalha800@gmail.com>
2022-04-23 16:18:49 -0300

    Refactoring: Extrair Constante
    Mau-cheiro identificado: Utilização de variável temporária para armazenar resultado de uma expressão, Números Mágicos, Modificação de variável temporária diversas vezes fora de um loop
    
    Descrição da refatoração: Trocar variável temporária por consulta, Extrair em Números mágicos em constantes, Criação de variável temporária para cada situação
    
    Classes/métodos/atributos afetados:
    * Método computar passou a utilizar consultas ao invés de utilizar variáveis temporárias
    * Classe ApuracaoImposto números mágicos foram substituídos por constantes
    * Atributo base foi dividido em diversos atributos, um para cada faixa
    
    Co-authored-by: giovannabbottino <giovannabbottino@gmail.com>
    Co-authored-by: André Lucas <andre.lucas.lemos@hotmail.com>
    Co-authored-by: Paulo Batista <matpaulo@hotmail.com.br>

--- tp1/src/model/ApuracaoImposto.java
+++ tp1/src/model/ApuracaoImposto.java
@@ -1,42 +1,43 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ApuracaoImposto {
+	private static final float LIMITE_QUARTA_FAIXA = 913.63f;
+	private static final float LIMITE_TERCEIRA_FAIXA = 924.40f;
+	private static final float LIMITE_SEGUNDA_FAIXA = 922.67f;
+	private static final float LIMITE_PRIMEIRA_FAIXA = 1903.98f;
+	private static final float ALIQUOTA_PRIMEIRA_FAIXA = 7.5f/100;
+	private static final float ALIQUOTA_SEGUNDA_FAIXA = 15f/100;
+	private static final float ALIQUOTA_TERCEIRA_FAIXA = 22.5f/100;
+	private static final float ALIQUOTA_QUARTA_FAIXA = 27.5f/100;
 	private Simulador simulador;
-	private Imposto temp;
-	private float base;
-	//private List<Imposto> impostos;
+	private float base1aFaixa;
+	private float base2aFaixa;
+	private float base3aFaixa;
+	private float base4aFaixa;
+	private float base5aFaixa;
 
 	public ApuracaoImposto(Simulador fonte) {
 		this.simulador = fonte;
-		//this.impostos = new ArrayList<Imposto>();
+		this.base1aFaixa = this.simulador.getBaseCalculo();
+		this.base2aFaixa = base1aFaixa - LIMITE_PRIMEIRA_FAIXA;
+		this.base3aFaixa = base2aFaixa - LIMITE_SEGUNDA_FAIXA;
+		this.base4aFaixa = base3aFaixa - LIMITE_TERCEIRA_FAIXA;
+		this.base5aFaixa = base4aFaixa - LIMITE_QUARTA_FAIXA;
 	}
 	
 	public void computar(){		
-		this.base = this.simulador.getBaseCalculo();
-		if (this.base > 1903.98f){
+		if(this.base1aFaixa > LIMITE_PRIMEIRA_FAIXA){
             // 2a faixa
-            this.base -= 1903.98f;
-            this.temp = new Imposto((Math.min(this.base, 922.67f) * 7.5f/100), 2); 
-            this.simulador.addImposto(this.temp);
-            if(this.base > 922.67f) {
+            this.simulador.addImposto(new Imposto((Math.min(this.base2aFaixa, LIMITE_SEGUNDA_FAIXA) * ALIQUOTA_PRIMEIRA_FAIXA), 2));
+            if(this.base2aFaixa > LIMITE_SEGUNDA_FAIXA) {
                 // 3a faixa
-                this.base -= 922.67f;
-                this.temp = new Imposto((Math.min(this.base, 924.40f) * 15f/100), 3); 
-                this.simulador.addImposto(this.temp);
-                if (this.base > 924.40f) {
+                this.simulador.addImposto(new Imposto((Math.min(this.base3aFaixa, LIMITE_TERCEIRA_FAIXA) * ALIQUOTA_SEGUNDA_FAIXA), 3));
+                if (this.base3aFaixa > LIMITE_TERCEIRA_FAIXA) {
                     // 4a faixa
-                    this.base -= 924.40f;
-                    this.temp = new Imposto((Math.min(this.base, 913.63f) * 22.5f/100), 4);
-                    this.simulador.addImposto(this.temp);
-                    if(this.base > 913.63f) {
+                    this.simulador.addImposto(new Imposto((Math.min(this.base4aFaixa, LIMITE_QUARTA_FAIXA) * ALIQUOTA_TERCEIRA_FAIXA), 4));
+                    if(this.base4aFaixa > LIMITE_QUARTA_FAIXA) {
                         // 5a faixa
-                        this.base -= 913.63f;
-                        this.temp = new Imposto(( this.base * 27.5f/100), 5);
-                        this.simulador.addImposto(this.temp);
-                        
+                        this.simulador.addImposto(new Imposto((this.base5aFaixa * ALIQUOTA_QUARTA_FAIXA), 5));    
                     }
                 }
             }    
